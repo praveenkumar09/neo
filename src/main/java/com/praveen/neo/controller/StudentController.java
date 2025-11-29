66e57f423ccf328c0b1385178229de22614dd4a9
praveenkumar09 <tpkprav@gmail.com>
2025-11-29 19:04:29 +0800

    Created the services and controller classes

---
+++ src/main/java/com/praveen/neo/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.praveen.neo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+}
---
+++ src/main/java/com/praveen/neo/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.praveen.neo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+}
